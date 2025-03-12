0c9668d9a41984c7ec24eef87958df8666873a5d
HomeChum <admin@homechum.com>
2025-03-12 15:02:44 +0000

    Feature Update: detecting public charging

--- custom_components/homechum_ev_charging_tracker/binary_sensor.py
+++ custom_components/homechum_ev_charging_tracker/binary_sensor.py
@@ -6,23 +6,31 @@ from homeassistant.core import HomeAssistant
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_entry(hass, config_entry, async_add_entities):
+async def async_setup_platform(hass, config_entry, async_add_entities, discovery_info=None):
     """Set up binary sensor entities from a config entry."""
     async_add_entities([PublicChargingDetectedSensor(hass)])
 
 class PublicChargingDetectedSensor(BinarySensorEntity):
     """Binary sensor: Public Charging Detected."""
 
-    _attr_name = "Public Charging Detected"
-
     def __init__(self, hass: HomeAssistant):
         self.hass = hass
+        self._attr_name = "Public Charging Detected"
+        self._attr_unique_id = "ev_public_charge_detected"
+        self._attr_state = False  # Default state is False
 
     @property
     def is_on(self):
-        cable_state = self.hass.states.get("binary_sensor.myida_charging_cable_connected")
+        """Determine if public charging is detected."""
+        charging_state = self.hass.states.get("switch.myida_charging")
+        location_state = self.hass.states.get("device_tracker.myida_position")
         ohme_status = self.hass.states.get("sensor.ohme_epod_status")
-        if cable_state is None or ohme_status is None:
-            return False
-        # A connected cable and an "unplugged" Ohme status indicate a public charger is detected.
-        return cable_state.state == "on" and ohme_status.state == "unplugged"
+
+        if not charging_state or not location_state or not ohme_status:
+            return False  # If any state is unavailable, assume no public charging
+
+        return (
+            charging_state.state == "on" and
+            location_state.state != "home" and
+            ohme_status.state == "unplugged"
+        )
